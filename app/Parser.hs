df82d67fbef2837027f3b1ed8a0ed190124fdeb3
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2024-02-21 13:20:48 +0200

    finish associativity, precedence and ifthenelse

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,48 +1,125 @@
 module Parser where
 
-import Prelude
-
-import Tokenizer
+import Control.Monad.Except (ExceptT, runExceptT, throwError)
+import Control.Monad.Identity
+import Control.Monad.State.Lazy (StateT, evalStateT, get, put, runStateT)
 import Data.Int (Int64)
 import Data.List (uncons)
-import Debug.Trace (trace)
+import Debug.Trace (trace, traceM)
+import Tokenizer
+  ( Location,
+    Token (Identifier, IntegerLiteral, Operator, Punctuation),
+  )
+import Prelude
 
-data AST = IntegerLiteralAST Int64 | BooleanLiteralAST Bool | UnitAST | IdentifierAST String | Apply AST AST deriving (Eq, Show)
+data AST = IntegerLiteralAST Int64 | BooleanLiteralAST Bool | UnitAST | IdentifierAST String | Apply AST AST | IfAST AST AST AST deriving (Eq, Show)
 
 mkBinaryApply :: String -> AST -> AST -> AST
 mkBinaryApply op left right = Apply (Apply (IdentifierAST op) left) right
 
+precedence :: String -> Int
+precedence "+" = 1
+precedence "-" = 1
+precedence "*" = 2
+precedence "/" = 2
 
--- return an AST and the number of tokens consumed
+type TokenStream = ([(Token, Location)], Int)
 
-parseExpr :: [(Token, Location)] -> ((AST, Location), Int)
+type ASTStream = [(AST, Location)]
 
-parseExpr tokens = let 
-        ((firstAST, firstLoc), consumed) = parseValue tokens
-        associative left consumed rest = 
-            case uncons rest of
-                Just ((Operator op, loc), rest2) -> 
-                    let ((secondAST, _), consumed2) = parseValue rest2
-                    in associative (mkBinaryApply op left secondAST) (consumed + 1 + consumed2) (drop consumed2 rest2)
-                _ -> ((left, firstLoc), consumed)
-    in 
-        associative firstAST consumed (drop consumed tokens) 
+type Parser a = StateT TokenStream (ExceptT String Identity) a
 
-parseValue :: [(Token, Location)] -> ((AST, Location), Int)
--- integers
-parseValue ((IntegerLiteral n, loc) : _) = ((IntegerLiteralAST n, loc), 1)
-parseValue ((Operator "-", loc) : (IntegerLiteral n, _) : _ ) = ((IntegerLiteralAST (-n), loc), 2) 
+peek :: Parser (Maybe (Token, Location))
+peek = do
+  (tokens, _) <- get
+  case tokens of
+    [] -> return Nothing
+    (token : _) -> return $ Just token
 
--- identifiers
-parseValue ((Identifier "true", loc) : _ ) = ((BooleanLiteralAST True, loc), 1)
-parseValue ((Identifier "false", loc) : _ ) = ((BooleanLiteralAST False, loc), 1)
-parseValue ((Identifier "unit", loc) : _ ) = ((UnitAST, loc), 1)
-parseValue ((Identifier id, loc) : _) = ((IdentifierAST id, loc), 1)
+peekValue :: Parser (AST, Location)
+peekValue = do
+  state <- get
+  value <- parseValue
+  put state
+  return value
 
-parseValue ((token, loc) : _ ) = error $ "Parse error at " ++ (show loc) ++ " with token " ++ (show token)
+consume :: Parser (Token, Location)
+consume = do
+  (tokens, consumed) <- get
+  put (tail tokens, consumed + 1)
+  return (head tokens)
 
-parse :: [(Token, Location)] -> [(AST, Location)]
-parse [] = []
-parse tokens = [ast] ++ parse rest
-    where (ast, consumed) = parseExpr tokens
-          rest = drop consumed tokens
+isEmpty :: Parser Bool
+isEmpty = do
+  (tokens, _) <- get
+  return $ null tokens
+
+parseExpr :: Parser (AST, Location)
+parseExpr = do
+  nextToken <- peek
+  case nextToken of
+    Nothing -> throwError "Parse error: unexpected end of input"
+    Just (_, loc) -> do
+      ast <- parseExpr' 0
+      return (ast, loc)
+  where
+    parseExpr' :: Int -> Parser AST
+    parseExpr' prec = do
+      (left, _) <- parseValue
+      let loop left = do
+            nextToken <- peek
+            case nextToken of
+              Just (Operator op, _) ->
+                if precedence op >= prec
+                  then do
+                    consume
+                    right <- parseExpr' $ precedence op + 1
+                    loop $ mkBinaryApply op left right
+                  else return left
+              _ -> return left
+      loop left
+
+parseValue :: Parser (AST, Location)
+parseValue = do
+  (token, loc) <- consume
+  case token of
+    IntegerLiteral n -> return (IntegerLiteralAST n, loc)
+    Operator "-" -> do
+      (token, loc) <- consume
+      case token of
+        IntegerLiteral n -> return (IntegerLiteralAST (-n), loc)
+        _ -> throwError $ "Parse error at " ++ show loc ++ " with token " ++ show token
+    Identifier "true" -> return (BooleanLiteralAST True, loc)
+    Identifier "false" -> return (BooleanLiteralAST False, loc)
+    Identifier "unit" -> return (UnitAST, loc)
+    Identifier "if" -> do
+      (cond, _) <- parseExpr
+      consume -- then
+      (thenBranch, _) <- parseExpr
+      elseKeyword <- peek
+      elseBranch <- case elseKeyword of
+        Just (Identifier "else", _) -> do
+          consume
+          (expr, _) <- parseExpr
+          return expr
+        _ -> return UnitAST
+      return (IfAST cond thenBranch elseBranch, loc)
+    Identifier id -> return (IdentifierAST id, loc)
+    Punctuation "(" -> do
+      expr <- parseExpr
+      consume
+      return expr
+    _ -> throwError $ "Parse error at " ++ show loc ++ " with token " ++ show token
+
+parse :: [(Token, Location)] -> Either String [(AST, Location)]
+parse tokens = runIdentity $ runExceptT $ evalStateT parse' (tokens, 0)
+  where
+    parse' :: Parser ASTStream
+    parse' = do
+      empty <- isEmpty
+      if empty
+        then return []
+        else do
+          (ast, loc) <- parseExpr
+          rest <- parse'
+          return $ (ast, loc) : rest
--- test/TestParser.hs
+++ test/TestParser.hs
@@ -1,69 +1,133 @@
 module TestParser where
 
+import Data.Bool
+import Parser
+import Test.Falsify.Generator qualified as Gen
+import Test.Falsify.Predicate as P
+import Test.Falsify.Range qualified as Range
 import Test.Tasty
 import Test.Tasty.Falsify
-import Test.Falsify.Generator qualified as Gen
-import Test.Falsify.Range qualified as Range
-import Test.Falsify.Predicate as P
-import Test.Tasty.HUnit (assertEqual, testCase)
-
-import Tokenizer (Token(..), Location(..), tokenize)
-import Parser
-import Data.Bool
+import Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
+import Tokenizer (Location (..), Token (..), tokenize)
 
 parserTests = testGroup "Parser" [propertyTests, unitTests]
 
-propertyTests = testGroup "Property tests"
-    [
-        testProperty "Positive integer literal" $
-            do
-                value <- gen $ Gen.inRange $ Range.between (0, maxBound)
-                let literal = show value
-                let tokens = tokenize literal
-                    ast = parse tokens
-                assert $ P.expect 1 .$ ("number of tokens", length tokens) 
-                assert $ P.expect (IntegerLiteralAST value) .$ ("ast", fst $ head ast),
-
-        testProperty "Negative integer literal" $
-            do
-                value <- gen $ Gen.inRange $ Range.between (0, maxBound)
-                let literal = show value
-                let tokens = tokenize $ "-" ++ literal
-                    ast = parse tokens
-                assert $ P.expect (IntegerLiteralAST (-value)) .$ ("ast", fst $ head ast)
+propertyTests =
+  testGroup
+    "Property tests"
+    [ testProperty "Positive integer literal" $
+        do
+          value <- gen $ Gen.inRange $ Range.between (0, maxBound)
+          let literal = show value
+          let tokens = tokenize literal
+          ast <- case parse tokens of
+            Left err -> testFailed err
+            Right result -> return result
+          assert $ P.expect 1 .$ ("number of tokens", length tokens)
+          assert $ P.expect (IntegerLiteralAST value) .$ ("ast", fst $ head ast),
+      testProperty "Negative integer literal" $
+        do
+          value <- gen $ Gen.inRange $ Range.between (0, maxBound)
+          let literal = show value
+          let tokens = tokenize $ "-" ++ literal
+          ast <- case parse tokens of
+            Left err -> testFailed err
+            Right result -> return result
+          assert $ P.expect (IntegerLiteralAST (-value)) .$ ("ast", fst $ head ast)
     ]
 
-unitTests = testGroup "Unit tests"
-    [
-        testCase "Boolean true literal" $
-            do assertEqual "" 
-                [(BooleanLiteralAST True, Location 0 0)]
-                (parse $ [(Identifier "true", Location 0 0)]),
+parseSuccess :: [(Token, Location)] -> IO [(AST, Location)]
+parseSuccess tokens = do
+  case parse tokens of
+    Right ast -> return ast
+    Left err -> assertFailure err
 
-        testCase "Boolean false literal" $
-            do assertEqual "" 
-                [(BooleanLiteralAST False, Location 0 0)]
-                (parse $ [(Identifier "false", Location 0 0)]),
-
-        testCase "Unit value" $
-            do assertEqual "" 
-                [(UnitAST, Location 0 0)]
-                (parse $ [(Identifier "unit", Location 0 0)]),
-
-        testCase "Addition with integers" $
-            do assertEqual ""
-                [(Apply (Apply (IdentifierAST "+") (IntegerLiteralAST 1)) (IntegerLiteralAST 2), Location 0 0)]
-                (parse $ tokenize "1 + 2"),
-
-        testCase "Addition with identifiers" $
-            do assertEqual ""
-                [(Apply (Apply (IdentifierAST "+") (IdentifierAST "a")) (IdentifierAST "b"), Location 0 0)]
-                (parse $ tokenize "a + b"),
-
-        testCase "Left associative addition" $
-            do 
-                let onePlusTwo = Apply (Apply (IdentifierAST "+") (IntegerLiteralAST 1)) (IntegerLiteralAST 2)
-                assertEqual ""
-                    [(Apply (Apply (IdentifierAST "+") onePlusTwo) (IntegerLiteralAST 3), Location 0 0)]
-                    (parse $ tokenize "1 + 2 + 3")
+unitTests =
+  testGroup
+    "Unit tests"
+    [ testCase "Boolean true literal" $
+        do
+          ast <- parseSuccess $ [(Identifier "true", Location 0 0)]
+          assertEqual
+            ""
+            [(BooleanLiteralAST True, Location 0 0)]
+            ast,
+      testCase "Boolean false literal" $
+        do
+          ast <- parseSuccess $ [(Identifier "false", Location 0 0)]
+          assertEqual
+            ""
+            [(BooleanLiteralAST False, Location 0 0)]
+            ast,
+      testCase
+        "Unit value"
+        $ do
+          ast <- parseSuccess $ [(Identifier "unit", Location 0 0)]
+          assertEqual
+            ""
+            [(UnitAST, Location 0 0)]
+            ast,
+      testCase "Addition with integers" $
+        do
+          ast <- parseSuccess $ tokenize "1 + 2"
+          assertEqual
+            ""
+            [(mkBinaryApply "+" (IntegerLiteralAST 1) (IntegerLiteralAST 2), Location 0 0)]
+            ast,
+      testCase "Addition with identifiers" $
+        do
+          ast <- parseSuccess $ tokenize "a + b"
+          assertEqual
+            ""
+            [(mkBinaryApply "+" (IdentifierAST "a") (IdentifierAST "b"), Location 0 0)]
+            ast,
+      testCase "Left associative addition of three numbers" $
+        do
+          let onePlusTwo = mkBinaryApply "+" (IntegerLiteralAST 1) (IntegerLiteralAST 2)
+          ast <- parseSuccess $ tokenize "1 + 2 + 3"
+          assertEqual
+            ""
+            [(mkBinaryApply "+" onePlusTwo (IntegerLiteralAST 3), Location 0 0)]
+            ast,
+      testCase "Left associative addition of four numbers" $
+        do
+          let onePlusTwo = mkBinaryApply "+" (IntegerLiteralAST 1) (IntegerLiteralAST 2)
+          let onePlusTwoPlusThree = mkBinaryApply "+" onePlusTwo (IntegerLiteralAST 3)
+          ast <- parseSuccess $ tokenize "1 + 2 + 3 + 4"
+          assertEqual
+            ""
+            [(mkBinaryApply "+" onePlusTwoPlusThree (IntegerLiteralAST 4), Location 0 0)]
+            ast,
+      testCase "Multiply precedence over addition" $
+        do
+          let twoTimesThree = mkBinaryApply "*" (IntegerLiteralAST 2) (IntegerLiteralAST 3)
+          ast <- parseSuccess $ tokenize "1 + 2 * 3"
+          assertEqual
+            ""
+            [(mkBinaryApply "+" (IntegerLiteralAST 1) twoTimesThree, Location 0 0)]
+            ast,
+      testCase "Parentheses precedence" $
+        do
+          let aPlusB = mkBinaryApply "+" (IdentifierAST "a") (IdentifierAST "b")
+          ast <- parseSuccess $ tokenize "(a + b) * c"
+          assertEqual
+            ""
+            [(mkBinaryApply "*" aPlusB (IdentifierAST "c"), Location 0 0)]
+            ast,
+      testCase "if then else" $
+        do
+          let ifThenElse = IfAST (BooleanLiteralAST True) (IntegerLiteralAST 1) (IntegerLiteralAST 2)
+          ast <- parseSuccess $ tokenize "if true then 1 else 2"
+          assertEqual
+            ""
+            [(ifThenElse, Location 0 0)]
+            ast,
+      testCase "if then" $
+        do
+          let ifThen = IfAST (BooleanLiteralAST True) (IntegerLiteralAST 1) UnitAST
+          ast <- parseSuccess $ tokenize "if true then 1"
+          assertEqual
+            ""
+            [(ifThen, Location 0 0)]
+            ast
     ]
